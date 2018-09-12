f3e01e8ae73bc7cd9e9fa4f5a67fec4ef45e234b
Yongyan Liu <yongyanliu17@gmail.com>
2018-09-11 21:18:54 -0500

    pom.xml of LocationSimulatorRestApi complete

---
+++ locationsimulator/src/main/java/com/yongyan/LocationSimulatorApplication.java
@@ -0,0 +1,11 @@
+package com.yongyan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationSimulatorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LocationSimulatorApplication.class, args);
+    }
+}
