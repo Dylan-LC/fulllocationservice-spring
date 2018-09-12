c01daf04d3def71949820aef0b029c70989d1c22
Yongyan Liu <yongyanliu17@gmail.com>
2018-09-12 02:27:14 -0500

    locationingest service works

---
+++ locationingest/src/main/java/com/yongyan/LocationIngestApplication.java
@@ -0,0 +1,13 @@
+package com.yongyan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationIngestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocationIngestApplication.class);
+    }
+
+}
